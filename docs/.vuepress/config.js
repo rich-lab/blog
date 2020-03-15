2b4b12729a38c7d1dea34ba143abcd06ef499e05
ULIVZ <472590061@qq.com>
2020-03-15 11:18:41 +0800

    feat: enhance logo & theme color.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,8 +15,12 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
+    ['link', {
+      rel: 'icon',
+      href: 'https://gw.alipayobjects.com/zos/finxbff/compress-tinypng/7a6ec1c9-2680-4817-8748-eeab10cb1469.png'
+    }],
     ['meta', { name: 'referrer', content: 'no-referrer' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#272727' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
